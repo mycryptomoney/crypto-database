bc73f967c425f315ae3e3363a27fbe48566d1051
Alexei Murinets <62135607+AlexMurinets@users.noreply.github.com>
2021-12-08 14:43:17 +0200

    Changed some tables

--- src/main/resources/db/migration/V202__insert_currency.sql
+++ src/main/resources/db/migration/V202__insert_currency.sql
@@ -1,4 +1,4 @@
-INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Dollar', 'USD', 1, 1000000, true);
+INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Dollar', 'USDT', 1, 1000000, true);
 INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Euro', 'EUR', 1.2, 1000000, false);
-INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Bitcoin', 'BTC', 60000, 1000000, false);
-INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Ethereum', 'ETH', 5000, 1000000, false);
+INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Bitcoin', 'BTC', 60000, 1000000, true);
+INSERT INTO currency (name, abbreviation, value, capitalisation, activated) VALUES ('Ethereum', 'ETH', 5000, 1000000, true);
--- src/main/resources/db/migration/V203__insert_wallet.sql
+++ src/main/resources/db/migration/V203__insert_wallet.sql
@@ -1,4 +1,5 @@
-INSERT INTO wallet (amount, currency_id, user_id) VALUES (10000, 2, 1);
-INSERT INTO wallet (amount, currency_id, user_id) VALUES (5000, 1, 1);
+INSERT INTO wallet (amount, currency_id, user_id) VALUES (5000, 2, 1);
+INSERT INTO wallet (amount, currency_id, user_id) VALUES (1000000, 1, 1);
 INSERT INTO wallet (amount, currency_id, user_id) VALUES (1000, 2, 2);
+INSERT INTO wallet (amount, currency_id, user_id) VALUES (10, 3, 1);
 
